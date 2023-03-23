be8e40dffb76a725cdf27c9f4847a232bb3cbc46
David Beylin <57668741+davabey@users.noreply.github.com>
2023-03-23 14:14:12 +0500

    какая-то хрень

--- workers/admin.py
+++ workers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
 
 # class WorkerInOrderInline(admin.TabularInline):
@@ -14,6 +15,20 @@ from .models import *
 #     class Meta:
 #         model = Order
 
+#TEST
+class UserInline(admin.StackedInline):
+    model = Worker
+    can_delete = False
+    verbose_name_plural = 'Доп. информация'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+#TEST
 
 admin.site.register(Worker)
 
